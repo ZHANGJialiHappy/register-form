ef83aed2abc4f93803b900aa73bbc7cb8bc5be3d
Jiali Zhang <zhangjiali86@gmail.com>
2022-11-20 18:50:37 +0100

    feat: create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Login } from './features/login/Login';
 import { Register } from './features/register/Register';
+
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
