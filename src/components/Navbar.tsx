a39a8ad13a7a1e3a247728cc477acd3a2b48ad6d
Jiali Zhang <zhangjiali86@gmail.com>
2022-11-22 14:36:06 +0100

    feat: create Navbar with tailwind css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,17 @@ import {Link} from "react-router-dom";
 export const Navbar = () => {
     return (
         <div>
-            Navbar
-            <div><Link to="/register">Register Form</Link></div>
-            <div><Link to="/login">Login Form</Link></div>
+            <div className="navbar bg-base-100 justify-end">
+
+            <Link className="btn btn-ghost normal-case text-xl" to="/register">
+            Register Form
+            </Link>
+
+            <Link className="btn btn-ghost normal-case text-xl" to="/login">
+            Login Form
+            </Link>
+
+            </div>
         </div>
     )
     
