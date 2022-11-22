0a213859edc96fd996b521e6cfa1ad3de5bc3a04
Jiali Zhang <zhangjiali86@gmail.com>
2022-11-22 12:31:45 +0100

    feat: create login form

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,8 +1,48 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 export const Login = () => {
 
+    const schema = yup.object().shape({
+        email: yup.string().email().required("required"),
+        password: yup.string().min(6).max(20).required(),
+    })
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
+
     return (
-        <div className="text-3xl font-bold underline text-red-600">
-            Login Form
+        <div className="m-5 flex flex-row justify-center">
+        <form className="card w-96 bg-base-100 shadow-xl gap-5 p-5" onSubmit={handleSubmit(onSubmit)}>
+
+            <div className="form-control">
+                <label className="input-group input-group-vertical">
+                    <span>Email</span>
+                    <input type="text" placeholder="info@site.com" 
+                    className={`input input-bordered ${errors.email?.message && "input-error"}`}  required {...register("email")} />
+                </label>
+                <p className="">{errors.email?.message?.toString()}</p>
+            </div>
+
+            <div className="form-control">
+                <label className="input-group input-group-vertical">
+                    <span>Password</span>
+                    <input type="password" placeholder="Password..." 
+                    className={`input input-bordered ${errors.password?.message && "input-error"}`} required {...register("password")} />
+                </label>
+                <p className="">{errors.password?.message?.toString()}</p>
+            </div>
+
+            <div className="flex justify-end">
+                <input className="btn" type="submit" />
+            </div>
+        </form>
         </div>
     )
 }
